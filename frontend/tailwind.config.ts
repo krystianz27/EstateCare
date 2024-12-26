f4e011859cf2baae719fc004b6610a72ed69bfb8
Krystian <226311@student.uek.krakow.pl>
2024-12-26 21:33:29 +0100

    chore: add home and not-found pages

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -1,62 +1,42 @@
 import type { Config } from "tailwindcss";
 import tailwindcssAnimate from "tailwindcss-animate";
 
-const config: Config = {
+const config = {
   darkMode: ["class"],
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}",
+    "./app/**/*.{ts,tsx}",
+    "./src/**/*.{ts,tsx}",
   ],
+  prefix: "",
   theme: {
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1400px",
+      },
+    },
     extend: {
       colors: {
-        // background: 'hsl(var(--background))',
-        // foreground: 'hsl(var(--foreground))',
-        // card: {
-        // 	DEFAULT: 'hsl(var(--card))',
-        // 	foreground: 'hsl(var(--card-foreground))'
-        // },
-        // popover: {
-        // 	DEFAULT: 'hsl(var(--popover))',
-        // 	foreground: 'hsl(var(--popover-foreground))'
-        // },
-        // primary: {
-        // 	DEFAULT: 'hsl(var(--primary))',
-        // 	foreground: 'hsl(var(--primary-foreground))'
-        // },
-        // secondary: {
-        // 	DEFAULT: 'hsl(var(--secondary))',
-        // 	foreground: 'hsl(var(--secondary-foreground))'
-        // },
-        // muted: {
-        // 	DEFAULT: 'hsl(var(--muted))',
-        // 	foreground: 'hsl(var(--muted-foreground))'
-        // },
-        // accent: {
-        // 	DEFAULT: 'hsl(var(--accent))',
-        // 	foreground: 'hsl(var(--accent-foreground))'
-        // },
-        // destructive: {
-        // 	DEFAULT: 'hsl(var(--destructive))',
-        // 	foreground: 'hsl(var(--destructive-foreground))'
-        // },
-        // border: 'hsl(var(--border))',
-        // input: 'hsl(var(--input))',
-        // ring: 'hsl(var(--ring))',
-        // chart: {
-        // 	'1': 'hsl(var(--chart-1))',
-        // 	'2': 'hsl(var(--chart-2))',
-        // 	'3': 'hsl(var(--chart-3))',
-        // 	'4': 'hsl(var(--chart-4))',
-        // 	'5': 'hsl(var(--chart-5))'
-        // }
+        googleBlue: "#4285F4",
+        asparagus: "#60992D",
+        deepBlueGrey: "#263238",
+        gray: "rgba(125,144,201,0.34)",
+        lightGray: "#7B8EC8",
+        electricIndigo: "#6610F2",
+        richBlack: "#0D1317",
+        veryBlack: "#000000",
+        babyPowder: "#FBFEF9",
+        pumpkin: "#F17105",
+        platinum: "#DDE1E4",
+        lightGrey: "#EEEEEE",
+        pear: "#C2E812",
+        eerieBlack: "#171D1C",
+        ballonWhite: "#dfe6f6",
       },
-      borderRadius: {
-        lg: "var(--radius)",
-        md: "calc(var(--radius) - 2px)",
-        sm: "calc(var(--radius) - 4px)",
-      },
+
       keyframes: {
         "accordion-down": {
           from: { height: "0" },
@@ -79,4 +59,5 @@ const config: Config = {
   },
   plugins: [tailwindcssAnimate],
 } satisfies Config;
+
 export default config;
