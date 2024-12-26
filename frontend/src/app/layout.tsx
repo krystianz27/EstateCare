91dbac22e6d5cbd538ab83b72e9c5da7a7983fcc
Krystian <226311@student.uek.krakow.pl>
2024-12-26 18:39:56 +0100

    chore: eslint and prettier, custom fonts

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,17 +1,7 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
 import "./globals.css";
-
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
+import React from "react";
+import { openSans, robotoSlab } from "@/lib/font";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -24,9 +14,9 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${openSans.variable} ${robotoSlab.variable} antialiased`}
       >
         {children}
       </body>
--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,7 +1,8 @@
 export default function Home() {
   return (
     <div>
-      <h1 className="text-6xl">Home Page!!!</h1>
+      <h1 className="font-robotoSlab text-6xl">Home Page!!!</h1>
+      <p className="font-openSans text-3xl">OpenSans Font Test</p>
     </div>
   );
 }
