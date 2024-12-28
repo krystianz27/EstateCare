0217a9105aeef4a072b7740a3542c9a8928ee86d
Krystian <226311@student.uek.krakow.pl>
2024-12-28 20:26:59 +0100

    chore: add navbar

---
+++ frontend/src/components/shared/navbar/Navbar.tsx
@@ -0,0 +1,29 @@
+import { HomeModernIcon } from "@heroicons/react/24/solid";
+import React from "react";
+import Link from "next/link";
+
+export default function Navbar() {
+  return (
+    <nav className="flex-between bg-baby_rich border-b-platinum shadow-platinum fixed z-50 w-full gap-5 border-b-2 p-4 sm:p-6 lg:px-12 dark:border-b-0 dark:shadow-none">
+      <Link href="/" className="flex items-center">
+        <HomeModernIcon className="mr-2 size-11 text-lime-500" />
+        <p className="h2-bold font-robotoSlab text-veryBlack dark:text-babyPowder hidden sm:block">
+          Estate Care <span className="text-lime-500"> Apartments</span>
+        </p>
+      </Link>
+
+      <div className="flex items-center gap-4 sm:gap-6 lg:gap-8">
+        {/* placeholder theme switcher component */}
+        {/* <ThemeSwitcher /> */}
+        <div className="dark: text-pumpkin hidden text-lg sm:block">
+          Theme Switcher
+        </div>
+        {/* placeholder theme switcher component */}
+        {/* <MobileNavbar /> */}
+        <div className="dark: text-pumpkin text-lg sm:text-xl">
+          Mobile Navbar
+        </div>
+      </div>
+    </nav>
+  );
+}
