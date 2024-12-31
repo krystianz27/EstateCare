a2833352237c24de09c776fc0156b1e78ed60015
Krystian <226311@student.uek.krakow.pl>
2024-12-31 20:50:49 +0100

    feat: add Auth Header Component

---
+++ frontend/src/app/(estate)/(auth)/activate/[uid/[token]/page.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function ActivationPage() {
+  return (
+    <div>
+      <h1 className="dark: text-pumpkin text-6xl">Activate Page</h1>
+    </div>
+  );
+}
