9952f46e1c2d11b72224297ae2f4483dd9949496
Krystian <226311@student.uek.krakow.pl>
2025-01-01 18:47:32 +0100

    fix: corrected activation URL in authApiSlice.ts

--- frontend/src/app/(estate)/(auth)/activate/[uid/[token]/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function ActivationPage() {
-  return (
-    <div>
-      <h1 className="dark: text-pumpkin text-6xl">Activate Page</h1>
-    </div>
-  );
-}
---
+++ frontend/src/app/(estate)/(auth)/activate/[uid]/[token]/page.tsx
@@ -0,0 +1,70 @@
+"use client";
+
+import { useActivateUserMutation } from "@/lib/redux/features/auth/authApiSlice";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+import { toast } from "react-toastify";
+
+interface ActivationProps {
+  params: {
+    uid: string;
+    token: string;
+  };
+}
+
+export default function ActivationPage({ params }: ActivationProps) {
+  const router = useRouter();
+  const { uid, token } = params;
+
+  const [activateUser, { isLoading, isSuccess, isError, error }] =
+    useActivateUserMutation();
+
+  useEffect(() => {
+    activateUser({ uid, token });
+  }, [activateUser, uid, token]);
+
+  useEffect(() => {
+    if (isSuccess) {
+      toast.success("Account activated successfully!");
+      router.push("/login");
+    }
+    if (isError) {
+      toast.error("Failed to activate your account");
+    }
+  }, [isSuccess, isError, error, router]);
+
+  let messageContent;
+  if (isLoading) {
+    messageContent = (
+      <div className="flex-center">
+        <span className="mr-2">⏰</span>
+        <span>Activating your account....Please wait</span>
+        <span className="ml-2">🥱</span>
+      </div>
+    );
+  } else if (isSuccess) {
+    messageContent = (
+      <div>
+        <span className="mr-2">✅</span>
+        <span>Account Activated successfully!</span>
+      </div>
+    );
+  } else if (isError) {
+    messageContent = (
+      <div>
+        <span className="mr-2">❌</span>
+        <span>Your account has already been activated...</span>
+      </div>
+    );
+  }
+
+  return (
+    <div className="flex min-h-screen items-center justify-center">
+      <div className="text-center">
+        <h3 className="dark:text-platinum font-robotoSlab text-2xl font-bold text-gray-800 sm:text-4xl md:text-5xl">
+          {messageContent}
+        </h3>
+      </div>
+    </div>
+  );
+}
