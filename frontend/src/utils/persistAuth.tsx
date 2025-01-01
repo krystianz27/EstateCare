82b154d48e5c3757ca62a93c1264d75b4e9a53a4
Krystian <226311@student.uek.krakow.pl>
2025-01-01 22:24:30 +0100

    feat: add Persist Auth

---
+++ frontend/src/utils/persistAuth.tsx
@@ -0,0 +1,22 @@
+"use client";
+
+import { useAppDispatch } from "@/lib/redux/hooks/typedHooks";
+import { useEffect } from "react";
+import { getCookie } from "cookies-next";
+import { setAuth, setLogout } from "@/lib/redux/features/auth/authSlice";
+
+export default function PersistAuth() {
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    const isLoggedIn = getCookie("logged_in") === "true";
+
+    if (isLoggedIn) {
+      dispatch(setAuth());
+    } else {
+      dispatch(setLogout());
+    }
+  }, [dispatch]);
+
+  return null;
+}
