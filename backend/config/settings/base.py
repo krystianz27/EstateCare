66a76a499a53362006fead0410f7ba3d23055ea6
Krystian <226311@student.uek.krakow.pl>
2025-04-17 22:33:25 +0200

    feat: Argon2 Passowrd Hasher

--- backend/config/settings/base.py
+++ backend/config/settings/base.py
@@ -114,9 +114,10 @@ DATABASES = {
 }
 
 PASSWORD_HASHERS = [
+    "django.contrib.auth.hashers.Argon2PasswordHasher",
     "django.contrib.auth.hashers.PBKDF2PasswordHasher",
     "django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher",
-    "django.contrib.auth.hashers.Argon2PasswordHasher",
+    # "django.contrib.auth.hashers.Argon2PasswordHasher",
     "django.contrib.auth.hashers.BCryptSHA256PasswordHasher",
     "django.contrib.auth.hashers.ScryptPasswordHasher",
 ]
