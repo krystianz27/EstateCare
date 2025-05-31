5d6eee567e8374c23ae7bed7ff0c9ea1cdd1f082
Krystian <226311@student.uek.krakow.pl>
2025-06-01 00:59:42 +0200

    chore: DO deploy

--- do_server_deploy_safer.sh
+++ do_server_deploy_safer.sh
@@ -3,7 +3,6 @@
 set -e
 
 if [ -z "$DIGITAL_OCEAN_IP_ADDRESS" ]; then
-
   echo "Please set the DIGITAL_OCEAN_IP_ADDRESS environment variable"
   exit 1
 fi
@@ -15,35 +14,23 @@ cleanup_local() {
 
 trap cleanup_local EXIT
 
-echo "Packaging project files..."
+echo "Packaging project files (without .env.production)..."
 git archive --format tar --output ./project.tar main # Create project.tar without files and directories that are in .gitignore
 
 BACKEND_ENV="./backend/.envs/.env.production"
 FRONTEND_ENV="./frontend/.env.production"
 ARCHIVE="project.tar"
 
-# Archive backend/.envs.env.production
-if [ -f "$BACKEND_ENV" ]; then
-  echo "Adding $BACKEND_ENV to archive..."
-  tar --append --file="$ARCHIVE" -C . "backend/.envs/.env.production"
-else
-  echo "Warning: $BACKEND_ENV not found!"
-fi
-
-# Archive frontend/.env.production
-if [ -f "$FRONTEND_ENV" ]; then
-  echo "Adding $FRONTEND_ENV to archive..."
-  tar --append --file="$ARCHIVE" -C . "frontend/.env.production"
-else
-  echo "Warning: $FRONTEND_ENV not found!"
-fi
-
 echo "Archiving complete.
 
 echo "Started uploading project.tar to Digital Ocean..."
-
 # rsync -avz --progress ./project.tar root@$DIGITAL_OCEAN_IP_ADDRESS:/tmp/project.tar
 scp ./project.tar root@$DIGITAL_OCEAN_IP_ADDRESS:/tmp/project.tar
+
+echo "Uploading .env.production files..."
+scp ./backend/.envs/.env.production root@$DIGITAL_OCEAN_IP_ADDRESS:/tmp/backend.env.production
+scp ./frontend/.env.production root@$DIGITAL_OCEAN_IP_ADDRESS:/tmp/frontend.env.production
+
 
 echo "Building and deploying project..."
 
@@ -54,6 +41,8 @@ set -e
 cleanup_remote() {
     echo "Cleaning up remote files..."
     rm -rf /tmp/project.tar
+    rm -rf /tmp/backend.env.production
+    rm -rf /tmp/frontend.env.production
     rm -rf /app
 }
 
@@ -63,7 +52,15 @@ TEMP_DIR=$(mktemp -d)
 echo "Extracting project to $TEMP_DIR"
 tar -xf /tmp/project.tar -C "$TEMP_DIR"
 
-if [ -f "$TEMP_DIR/.envs/.env.production" ]; then
+echo "Placing .env.production files in the correct locations..."
+mkdir -p "$TEMP_DIR/backend/.envs"
+mv /tmp/backend.env.production "$TEMP_DIR/backend/.envs/.env.production"
+mv /tmp/frontend.env.production "$TEMP_DIR/frontend/.env.production"
+
+chmod 600 "$TEMP_DIR/backend/.envs/.env.production"
+chmod 600 "$TEMP_DIR/frontend/.env.production"
+
+if [ -f "$TEMP_DIR/backend/.envs/.env.production" ]; then
   echo ".env.production found, proceeding..."
 else
   echo "Warning: .env.production not found in extracted files"
